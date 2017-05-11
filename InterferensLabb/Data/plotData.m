ae83c5b4e0bafddd72f5ffe69e5b613e12d9123a
Anton Johansson <97antjoh@gmail.com>
2017-05-11 15:54:24 +0200

    Resultat, brödtext, figurer, oh my!

--- InterferensLabb/Data/plotData.m
+++ InterferensLabb/Data/plotData.m
@@ -1,4 +1,4 @@
-files = {'4-spalt data' 'dubbelspalt data' 'Edge data' 'enkelspalt data' 'tråd låg expo data' 'tråd hög expo data' 'tråd+enkelspalt data' 'Variabel enkelspalt 1 data' 'Variabel enkelspalt 2 data' 'Variabel enkelspalt 3 data'};
+files = {'4-spalt data' 'dubbelspalt data' 'enkelspalt data' 'trad lag expo data' 'trad hog expo data' 'trad+enkelspalt data' 'Variabel enkelspalt 1 data' 'Variabel enkelspalt 2 data' 'Variabel enkelspalt 3 data'};
 
 for i=1:length(files)
     file = files{i};
@@ -9,9 +9,7 @@ for i=1:length(files)
     hold on;
     grid on;
     box on;
-    if ~strcmp(file, 'Edge data')
-        plot(data(:,1), data(:,5), 'b');
-    end
+    plot(data(:,1), data(:,5), 'b');
     plot(data(:,1), data(:,2), 'r');
     hold off;
 
@@ -26,3 +24,25 @@ for i=1:length(files)
     %print(strcat('figurer/', file), '-depsc');
 end
 
+%% Edge data
+
+    file = 'Edge data';
+    data = importdata(file);
+
+    figure('Name', file);
+    
+    hold on;
+    grid on;
+    box on;
+    plot(data(:,1), data(:,2), 'r');
+    hold off;
+
+    h = legend('Experiment');
+    set(h,'Interpreter','latex');
+    
+    ylim([0 1]);
+    
+    xlabel('Avst\aa{}nd (mm)', 'Interpreter', 'latex');
+    ylabel('Normaliserad intensitet (W/m$^2$)', 'Interpreter', 'latex');
+    
+    %print(strcat('figurer/', file), '-depsc');
